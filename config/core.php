d0ecbaf2ba1062a96408c2301a1c622a5325b860
michaelshire <mike_shire@hotmail.com>
2017-11-03 15:33:51 -0400

    Update core.php

--- config/core.php
+++ config/core.php
@@ -11,7 +11,7 @@
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
  
 // set records or rows of data per page
-$records_per_page = 10;
+$records_per_page = 5;
  
 // calculate for the query LIMIT clause
 $from_record_num = ($records_per_page * $page) - $records_per_page;
